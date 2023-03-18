cc6a5ff02a85ffa60f1cf1147b27e97f4f9cbbfa
riofbr_ <rionugroho70@gmail.com>
2023-03-18 15:32:21 +0700

    Added create network

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@
 
 docker network rm hadoop-network
 
+docker network rm hadoop
+
 docker network create hadoop-network
+
+docker network create hadoop
 
 docker build --build-arg HADOOP_VERSION=$HADOOP_VERSION -t hadoop:$HADOOP_VERSION ./image/base/
