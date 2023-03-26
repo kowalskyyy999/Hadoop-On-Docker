7548c1163ff21c902e5aef2f86e3cdc304aebb20
kowalskyyy999 <rionugroho70@gmail.com>
2023-03-26 20:16:10 +0700

    Creating bigdata network

--- build.sh
+++ build.sh
@@ -2,12 +2,6 @@
 
 . ./.env
 
-docker network rm hadoop-network
-
-docker network rm hadoop
-
-docker network create hadoop-network
-
-docker network create hadoop
+docker network create bigdata
 
 docker build --build-arg HADOOP_VERSION=$HADOOP_VERSION -t hadoop:$HADOOP_VERSION ./image/base/
