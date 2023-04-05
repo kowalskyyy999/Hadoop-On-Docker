ac63f4725cc9e4358905e5265542b0886f62b885
kowalskyyy999 <rionugroho70@gmail.com>
2023-04-05 12:13:28 +0700

    Add args for build spark

--- build.sh
+++ build.sh
@@ -4,4 +4,8 @@
 
 docker network create bigdata
 
-docker build --build-arg HADOOP_VERSION=$HADOOP_VERSION -t hadoop:$HADOOP_VERSION ./image/base/
+docker build \
+	--build-arg HADOOP_VERSION=$HADOOP_VERSION \
+	--build-arg SPARK_VERSION=$SPARK_VERSION \
+	--build-arg SPARK_HADOOP_VERSION=$SPARK_HADOOP_VERSION \
+	-t hadoop:$HADOOP_VERSION ./image/base/
